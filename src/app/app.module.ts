9699cb2bae2a5bde23c5e502fde8dd6eae684b68
thien ban BUI <thienban91@hotmail.fr>
2018-01-18 15:50:02 +0100

    button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from "./app.component";
-import { DisconnectComponent } from "./shared/button/disconnect/disconnect.component";
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-/*const appRoutes: Routes = [
-  { path: "login", component: LoginPageComponent } // /page1 affiche le composant A
-];*/
+const appRoutes: Routes = [{ path: 'login', component: LoginPageComponent }];
 
 @NgModule({
-  declarations: [AppComponent, DisconnectComponent],
-  imports: [BrowserModule /*RouterModule.forRoot(appRoutes)*/],
+  declarations: [AppComponent, LoginPageComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/shared/button/disconnect/disconnect.component.ts
+++ src/app/shared/button/disconnect/disconnect.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-disconnect",
-  templateUrl: "./disconnect.component.html",
-  styleUrls: ["./disconnect.component.css"]
+  selector: 'app-disconnect',
+  templateUrl: './disconnect.component.html',
+  styleUrls: ['./disconnect.component.css']
 })
 export class DisconnectComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
-  disconnect(event) {}
+  disconnect(event) {
+    localStorage.removeItem('access_token');
+    this.router.navigateByUrl('login');
+  }
 }
