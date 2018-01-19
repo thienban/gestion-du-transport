098e4dc5edee37d07ea579f6bb88c0a8f10b0fde
AlexGeb <alex.behaghel@gmail.com>
2018-01-19 10:30:02 +0100

    ajout d'un logo, et ajout d'un guard pour les routes

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { LoginService } from './shared/services/login.service';
+
+@Injectable()
+export class AuthGuard implements CanLoad {
+  constructor(private loginSvc: LoginService, private router: Router) {}
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+    console.log('voila la route : ', route);
+    const role = this.loginSvc.userRole;
+    const requiredRoles: string[] = route.data.requiredRoles;
+    console.log('required roles : ',requiredRoles, 'current user role :',role);
+    if (requiredRoles.includes(role)) {
+      return true;
+    } else {
+      this.router.navigateByUrl('login');
+      return false;
+    }
+  }
+}
