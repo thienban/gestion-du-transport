072e36d7fd41eba88de5bf7ba53ae53042c3fe83
Mélodie MAUBOIS <maubois.melodie@orange.fr>
2018-01-25 09:58:05 +0100

    modifs

--- src/app/shared/services/chauffeur.service.ts
+++ src/app/shared/services/chauffeur.service.ts
@@ -4,17 +4,27 @@ import { Collaborateur } from '../../domain/Collaborateur';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { LoginService } from './login.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
 export class ChauffeurService {
   chauffeursBS: BehaviorSubject<Collaborateur[]> = new BehaviorSubject([]);
   chauffeurs: Collaborateur[];
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private jwt: JwtHelperService,
+    private loginSvc: LoginService
+  ) {}
 
   refreshData() {
     this.http
       .get<Collaborateur[]>(environment.endpoint + '/collaborateurs/chauffeurs')
+      .do(chauf => {
+        console.log('refresh reservations');
+        this.chauffeurs = chauf;
+      })
       .subscribe(ch => this.chauffeursBS.next(ch));
   }
 
