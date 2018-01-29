8dd93bf1f6988c0dee1b71877122e8f01daf2d47
Mélodie MAUBOIS <maubois.melodie@orange.fr>
2018-01-29 09:42:47 +0100

    liste collab reservations société

--- src/app/collaborateur/liste-annonces/liste-annonces.component.ts
+++ src/app/collaborateur/liste-annonces/liste-annonces.component.ts
@@ -11,30 +11,10 @@ export class ListeAnnoncesComponent implements OnInit {
   @Input() libelleListe;
   @Input() titre;
   @Input() reservations;
-  @Input() maxSize;
-  page;
-  startLimit;
-  itemPerPage = 5;
-  currentDate;
-  covoitEnCours;
-  covoitHisto;
+
   constructor(private modalService: NgbModal) {}
 
-  ngOnInit() {
-    this.currentDate = Date.now();
-
-    this.page = 1;
-
-    this.startLimit = 1;
-    this.maxSize = this.itemPerPage;
-  }
-
-  onChange() {
-    this.startLimit =
-      this.page * this.itemPerPage - this.itemPerPage + (this.page - 1);
-
-    this.maxSize = this.startLimit + this.itemPerPage;
-  }
+  ngOnInit() {}
 
   detailAnnonce(reservation) {
     const modalRef = this.modalService.open(DetailCovoiturageComponent);
