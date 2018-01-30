922be2f76de588f68123bd97465392b671e3658f
Clément Bauchet <clement.bauchet44@gmail.com>
2018-01-30 18:14:31 +0100

    Fin détail véhicule + marque & modèle strings

--- src/app/domain/VehiculeSociete.ts
+++ src/app/domain/VehiculeSociete.ts
@@ -1,8 +1,8 @@
 export class VehiculeSociete {
   constructor(
     public immatriculation: string,
-    public marque: { id: number; libelle: string },
-    public modele: { id: number; libelle: string },
+    public marque: string,
+    public modele: string,
     public categorie: { id: number; libelle: string },
     public nbPlaces: number,
     public photo: string,
