a1877467860e571de1fef7059e1dfa1f555fd150
Mélodie MAUBOIS <maubois.melodie@orange.fr>
2018-01-30 13:41:16 +0100

    caroussel vehicule

--- src/app/domain/VehiculeSociete.ts
+++ src/app/domain/VehiculeSociete.ts
@@ -1,9 +1,11 @@
 export class VehiculeSociete {
   constructor(
+    public id: number,
     public immatriculation: string,
     public nbPlaces: number,
     public marque: string,
     public modele: string,
-    public photo: string
+    public photo: string,
+    public disponible?: boolean
   ) {}
 }
